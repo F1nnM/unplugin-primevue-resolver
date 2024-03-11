2f1f9912e55a4ed1e3f5c945561d01e11b165d1d
Finn Melzer <github@mfinn.de>
2024-03-11 15:22:34 +0000

    Published 1.0.4 - Added support for ESM

--- dist/index.mjs
+++ dist/index.mjs
@@ -1,28 +1,4 @@
-"use strict";
-var __defProp = Object.defineProperty;
-var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
-var __getOwnPropNames = Object.getOwnPropertyNames;
-var __hasOwnProp = Object.prototype.hasOwnProperty;
-var __export = (target, all) => {
-  for (var name in all)
-    __defProp(target, name, { get: all[name], enumerable: true });
-};
-var __copyProps = (to, from, except, desc) => {
-  if (from && typeof from === "object" || typeof from === "function") {
-    for (let key of __getOwnPropNames(from))
-      if (!__hasOwnProp.call(to, key) && key !== except)
-        __defProp(to, key, { get: () => from[key], enumerable: !(desc = __getOwnPropDesc(from, key)) || desc.enumerable });
-  }
-  return to;
-};
-var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
-
 // index.ts
-var primevue_resolver_exports = {};
-__export(primevue_resolver_exports, {
-  PrimeVueResolver: () => PrimeVueResolver
-});
-module.exports = __toCommonJS(primevue_resolver_exports);
 var components = [
   "Accordion",
   "AccordionTab",
@@ -167,7 +143,6 @@ function PrimeVueResolver(options = {}) {
     }
   };
 }
-// Annotate the CommonJS export names for ESM import in node:
-0 && (module.exports = {
+export {
   PrimeVueResolver
-});
+};
